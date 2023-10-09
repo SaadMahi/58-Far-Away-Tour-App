b669917197dcdea5ab3dfe8d968ae4638f259012
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-09 21:01:53 +0530

    🚨 fix: infinite loop issue

--- src/components/CheckList/CheckList.jsx
+++ src/components/CheckList/CheckList.jsx
@@ -1,34 +1,46 @@
-import { useState } from "react";
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
 import SortList from "./SortList/SortList";
 
-function CheckList(props) {
+// eslint-disable-next-line react/prop-types
+function CheckList({ newItems, checkBoxHandler, deleteHandler }) {
+  const [pullNewItems, setPullNewItems] = useState([]);
+  const [sortBy, setSortBy] = useState("input");
 
+  useEffect(() => {
+    const sortedItemHandler = () => {
+      let sortedItems = [...newItems];
 
-const [pullNewItems, setPullNewItems] = useState([])
-console.log(pullNewItems)
+      if (sortBy === "input") sortedItems = newItems;
+      if (sortBy === "description")
+        sortedItems = newItems.sort((a, b) =>
+          a.description.localeCompare(b.description)
+        );
+      if (sortBy === "packed")
+        sortedItems = newItems.slice().sort((a, b) => {
+          return Number(a.packed) - Number(b.packed);
+        });
+      setPullNewItems(sortedItems);
+    };
 
-  const sortPullItems = function(data){
-    setPullNewItems(data);
-  }
+    sortedItemHandler();
+  }, [newItems, sortBy]);
 
   return (
     <div className="list">
       <ul>
         {pullNewItems.map((item) => (
           <li key={item.id}>
-          <input onChange={() => props.checkBoxHandler(item.id)} type="checkbox" />
-          <span style={item.packed ? {textDecoration : 'line-through'} : {}}>
-            {item.quantity} {item.description}
+            <input onChange={() => checkBoxHandler(item.id)} type="checkbox" />
+            <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+              {item.quantity} {item.description}
             </span>
-            <button onClick={() => props.deleteHandler(item.id)}>❌</button>
+            <button onClick={() => deleteHandler(item.id)}>❌</button>
           </li>
         ))}
       </ul>
 
-          <SortList items={props.newItems}
-          pullItems={sortPullItems}
-          />
-
+      <SortList setSortBy={setSortBy} />
     </div>
   );
 }
--- src/components/CheckList/SortList/SortList.jsx
+++ src/components/CheckList/SortList/SortList.jsx
@@ -1,27 +1,10 @@
-import React, { useState } from "react";
+/* eslint-disable react/prop-types */
 
-function SortList(props) {
-  const [sortBy, setSortBy] = useState("description");
-
-  //   now to sort accordingly we have to create new items, we won't be manipulating  the original items
-  let newItems = props.items;
-
-//   console.log(newItems)
-
-  let sortedItems;
-
-  if(sortBy === 'input') sortedItems = newItems;
-  if(sortBy === 'description') sortedItems = newItems.sort((a, b) =>  a.description.localeCompare(b.description));
-  if(sortBy === 'packed') sortedItems = newItems.slice().sort((a, b) => Number(a.packed) - Number(b.packed));
-
-    console.log(sortedItems)
-    props.pullItems(sortedItems)
-
-  
-
+function SortList({ setSortBy }) {
   return (
     <div className="actions">
-      <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+   
+      <select onChange={(e) => setSortBy(e.target.value)}>
         <option value="input">Sort by input order</option>
         <option value="description">Sort by description</option>
         <option value="packed">Sort by packed status</option>
